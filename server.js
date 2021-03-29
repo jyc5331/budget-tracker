db9008d6bcb83b86876a28eab1450bc679f11ca2
jyc5331 <jcosgrovecoding@gmail.com>
2021-03-29 12:59:58 -0400

    comment removed

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-//supply heroku with line 8 by using config vars
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
